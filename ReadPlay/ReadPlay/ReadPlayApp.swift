c31db0872507fec524f4d6a84df3ef91a00cc40e
Hayun218 <gkahsdl@icloud.com>
2023-10-25 01:17:23 +0900

    [Init] 초기셋팅

---
+++ ReadPlay/ReadPlay/ReadPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadPlayApp.swift
+//  ReadPlay
+//
+//  Created by yun on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ReadPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
