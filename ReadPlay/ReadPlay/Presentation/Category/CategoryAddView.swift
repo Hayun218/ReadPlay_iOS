41932734f6169e83764356e2cf1fd7a429824ac6
Hayun218 <gkahsdl@icloud.com>
2023-12-04 01:22:37 +0900

    [Feat/#9] 추가 로직 구현

--- ReadPlay/ReadPlay/Presentation/Category/CategoryAddView.swift
+++ ReadPlay/ReadPlay/Presentation/Category/CategoryAddView.swift
@@ -12,13 +12,7 @@ struct CategoryAddView: View {
   @StateObject var addVM = CategoryAddViewModel()
   @StateObject var dataController = DataController.shared
   @Environment(\.managedObjectContext) var managedObjectContext
-  @State var newVocabs: [Vocab] = [ ]
-  
-  init() {
-    let newVocab = Vocab(context: managedObjectContext)
-    newVocabs.append(newVocab)
-    print(newVocabs.count)
-  }
+  @State var newVocabs: [StaticVocab] = [.init(word_id: 0, word: "", meaning: ""), ]
   
   let screenHeight = UIScreen.main.bounds.size.height
   
@@ -41,8 +35,6 @@ struct CategoryAddView: View {
             TextField("", text: $addVM.categoryTitle)
               .placeholder(when: addVM.categoryTitle == "", placeholder: {
                 Text("단어장의 이름을 입력하세요")
-                  .customFont(.headline3)
-                  .foregroundStyle(.textWhite)
                   .opacity(0.8)
               })
               .customFont(.headline3)
@@ -71,10 +63,19 @@ struct CategoryAddView: View {
       
       
       ScrollView {
-        ForEach(Array(newVocabs.enumerated()), id: \.element) { idx, new in
-          editForm(idx: idx)
+        LazyVStack(spacing: 0) {
+          ForEach(newVocabs.indices, id: \.self) { idx in
+            editForm(idx: idx)
+          }
+          Text("단어를 연속해서 입력하세요")
+            .customFont(.body1)
+            .foregroundStyle(.textWhite)
+            .padding(.top, 40)
         }
       }
+      
+     
+      
     }
     .background(backGradient())
     .interactiveDismissDisabled()
@@ -101,28 +102,46 @@ extension CategoryAddView {
   }
   
   private func addNewVocabIfNeeded(_ idx: Int) {
-    if idx == newVocabs.count - 1 && !newVocabs[idx].word.isEmpty && !newVocabs[idx].meaning.isEmpty {
-      DispatchQueue.main.async {
-        let newVocab = Vocab(context: managedObjectContext)
-        // Set properties for the newVocab instance if needed
-        newVocabs.append(newVocab)
-      }
+    if idx == newVocabs.count - 1 && newVocabs[idx].word != "" && newVocabs[idx].meaning != "" {
+      newVocabs.append(.init(word_id: newVocabs.count-1, word: "", meaning: ""))
+      
     }
   }
   
   private func editForm(idx: Int) -> some View {
-    return VStack {
+    let wordBinding = Binding<String>(
+      get: { newVocabs[idx].word },
+      set: { newValue in
+        newVocabs[idx].word = newValue
+        if newVocabs.count-1 == idx {
+          addNewVocabIfNeeded(idx)
+        }
+      }
+    )
+    
+    let meaningBinding = Binding<String>(
+      get: { newVocabs[idx].meaning },
+      set: { newValue in
+        newVocabs[idx].meaning = newValue
+        if newVocabs.count-1 == idx {
+          addNewVocabIfNeeded(idx)
+        }
+      }
+    )
+    
+    return VStack(alignment: .center, spacing: 0) {
       HStack(alignment: .center) {
         Text("\(idx)")
         
         VStack(spacing: 6) {
-          TextField("단어", text: $newVocabs[idx].word)
+          
+          TextField("단어",  text: wordBinding)
             .frame(height: 32)
             .background {
               RoundedRectangle(cornerRadius: 4)
                 .foregroundStyle(.gray100)
             }
-          TextField("뜻", text: $newVocabs[idx].meaning)
+          TextField("뜻",  text: meaningBinding)
             .frame(height: 32)
             .background {
               RoundedRectangle(cornerRadius: 4)
@@ -131,18 +150,24 @@ extension CategoryAddView {
         }
         .customFont(.body1)
         .foregroundStyle(.textBlack)
-        .onChange(of: newVocabs[idx]) { oldValue, newValue in
-          addNewVocabIfNeeded(idx)
-        }
+        .padding(.leading, 20)
+        .padding(.trailing, 40)
+        .padding(.vertical, 12)
         
         Button {
           print("remove the idx")
         } label: {
           Image(systemName: "trash")
+            .font(.system(size: 20))
+            .foregroundStyle(.redButton)
         }
       }
       .padding(.horizontal, 20)
-
+      
+      Divider()
+        .frame(height: 1)
+        .background(.gray300)
+      
     }
     .frame(maxWidth: .infinity)
     .frame(height: 94)
